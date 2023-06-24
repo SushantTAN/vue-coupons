f2b9b3028679e7ce9ea4d2ff38c376c226cfc902
SushantTandukar <sushant.tandukar@logicabeans.com>
2023-06-25 00:03:27 +0545

    added redeem logic and modals

--- app.vue
+++ app.vue
@@ -2,16 +2,40 @@
   <Heading>Coupons</Heading>
 
   <div v-for="coupon in items" :key="coupon.id"
-    class="transition-colors duration-500 hover:bg-gray-100 rounded mb-4 overflow-hidden shadow-lg flex align items-center justify-between px-4">
+    class="relative transition-colors duration-500 hover:bg-gray-100 rounded mb-4 overflow-hidden shadow-lg flex align items-center justify-between px-4">
     <div class="pr-6 py-4">
       <div class="text-xl mb-2">{{ coupon.name }}</div>
     </div>
 
     <button :disabled="!coupon.canRedeem" @click="handleRedeemButton(coupon)"
       class="transition-colors duration-1000 bg-cyan-500 hover:bg-sky-700 disabled:bg-gray-300 text-white px-2 py-1 rounded">Redeem</button>
-    <Modal v-if="showModal" :modalData="coupon" :showModal="showModal">
-    </Modal>
+    
+      <div v-if="!coupon.canRedeem" class="transition-all duration-500 opacity-0 hover:opacity-100 absolute left-0 top-0 w-full h-full flex items-center justify-center bg-cyan-600 flex-col" >
+        <p>Your promo code is:</p>
+        <h3>{{ coupon.promoCode }}</h3>
+      </div>
   </div>
+
+  <Modal v-if="modalData.id !== null" :modalData="modalData" @resetModal="resetModal" @changeToRedeem="changeToRedeem">
+    </Modal>
+  <!-- Card Flip -->
+  <!-- <div class="card-flip">
+          <div class="flip">
+            <div class="front">
+              
+              <div style="background-color: green;">
+                aaaaaaaaaaaaa
+              </div>
+            </div>
+            <div class="back">
+              
+              <div style="background-color: red;">
+                vbbbbbbbbbbbbb
+              </div>
+            </div>
+          </div>
+        </div> -->
+  <!-- End Card Flip -->
 </template>
 
 
@@ -21,29 +45,47 @@ interface CouponType {
   id: number;
   name: string;
   canRedeem: boolean;
+  promoCode: string;
 }
 
 interface ModalDataType {
   id: number | null;
   name: string | null;
   canRedeem: boolean | null;
+  promoCode: string | null;
 }
 // const test = await useFetch("/api/coupon", {method: "POST", body: {test: 'test'}});
 const coupons = await useFetch("/api/coupon");
 // let items = ref(coupons.data)
 let items = ref<CouponType[]>([
-  { id: 1, name: 'test gdagdag cia u ', canRedeem: true, },
-  { id: 2, name: 'test 2', canRedeem: true, },
-  
+  { id: 1, name: 'test gdagdag cia u ', canRedeem: true, promoCode: "123wwd" },
+  { id: 2, name: 'test 2', canRedeem: true, promoCode: "123wwd" },
+
 ])
-
-const showModal = ref<boolean>(false);
-
-const modalData = ref<ModalDataType>({ id: null, name: null, canRedeem: null, })
+const modalData = ref<ModalDataType>({ id: null, name: null, canRedeem: null, promoCode: null })
 
 const handleRedeemButton = (item: ModalDataType) => {
-  showModal.value = !showModal.value;
   modalData.value = item;
+}
+
+const resetModal = (item: ModalDataType) => {
+  modalData.value = { id: null, name: null, canRedeem: null, promoCode: null };
+}
+
+const changeToRedeem = (id:number) => {
+  
+  let tempState = [...items.value];
+  const index = tempState.findIndex(item => item.id === id);
+
+  // If the index is -1, it means the item was not found
+  if (index === -1) {
+    console.log('Item not found!');
+    return;
+  }
+
+  // Replace the item at the found index with the new item
+  tempState[index] = {...tempState[index], canRedeem: false};
+  items.value = tempState;
 }
 
 </script>
--- components/Modal.vue
+++ components/Modal.vue
@@ -1,14 +1,36 @@
 <template>
-    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" @click="showModal.value = !showModal.value">
-        <div class="bg-white rounded-lg p-8">
-            <!-- Modal content --> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique impedit beatae fugiat eum perferendis cum placeat, aut at error quo provident voluptatum excepturi officia veniam, maiores possimus iusto sed voluptate!
+    <div class="fixed flex items-center justify-center h-full w-full top-0">
+        <div class="h-auto max-h-full bg-white rounded-lg p-8 z-10 w-full">
+            <!-- <span @click="emits('resetModal')">close</span> -->
+            <h2 class="font-medium text-xl capitalize">{{ modalData.name }}</h2>
+            <h3>Do you want to redeem this coupon?</h3>
+
+            <div class="flex justify-between pt-5 gap-1">
+                <button class="flex-1 transition-colors duration-1000 bg-cyan-600 hover:bg-sky-700 disabled:bg-gray-300 text-white px-4 py-1 rounded" @click="handleRedeem()">Yes</button>
+                <button class="flex-1 transition-colors duration-1000 bg-red-400 hover:bg-red-600 disabled:bg-gray-300 text-white px-4 py-1 rounded" @click="emits('resetModal')">Cancel</button>
+
+            </div>
+
             <slot></slot>
         </div>
+        <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" @click="emits('resetModal')">
+
+        </div>
+        
     </div>
 </template>
   
 <script setup>
-const { showModal } = defineProps(['showModal']);
+const { modalData } = defineProps(["modalData"]);
+
+
+const emits = defineEmits(['resetModal', 'changeToRedeem']);
+
+const handleRedeem = () => {
+    emits('changeToRedeem', modalData.id);
+    emits('resetModal');
+}
+
 
 </script>
   
