f302dc3c99a6153e3dbc38c106b27bd11c314062
ReganAtLogicabeans <regan.timsina@logicabeans.com>
2023-06-26 13:57:02 +0545

    wip:1

---
+++ api/error-mapper.ts
@@ -0,0 +1,101 @@
+import type { AxiosError } from "axios";
+
+// import { TokenUtils } from "@utils";
+
+export const StatusCode = {
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  RESOURCE_NOT_FOUND: 404,
+  METHOD_NOT_ALLOWED: 405,
+  CONFLICT: 409,
+  UNPROCESSABLE_ENTITY: 422,
+};
+
+export const Messages = {
+  CONFLICT: "conflict",
+  UNEXPECTED: "unexpected",
+  BAD_REQUEST: "bad_request",
+  NOT_ALLOWED: "not_allowed",
+  UNAUTHORIZED: "unauthorized",
+  GENERAL_NETWORK_ERROR: "general",
+  SERVER_NOT_RESPONDING: "server_not_responding",
+  NO_INTERNET_CONNECTION: "no_internet_connection",
+};
+
+function errorCodeMessage(code?: number): string {
+  switch (code) {
+    case StatusCode.UNAUTHORIZED:
+      return Messages.UNAUTHORIZED;
+    case StatusCode.RESOURCE_NOT_FOUND:
+      return Messages.GENERAL_NETWORK_ERROR;
+    case StatusCode.METHOD_NOT_ALLOWED:
+      return Messages.NOT_ALLOWED;
+    case StatusCode.FORBIDDEN:
+    case StatusCode.CONFLICT:
+      return Messages.CONFLICT;
+    case StatusCode.BAD_REQUEST:
+      return Messages.BAD_REQUEST;
+    default:
+      return Messages.UNEXPECTED;
+  }
+}
+
+// async function getNetInfo(): Promise<NetInfoState | undefined> {
+//   try {
+//     return await NetInfo.fetch("wifi");
+//   } catch (e) {
+//     return undefined;
+//   }
+// }
+
+export interface ErrorResponse {
+  errorMessage: string;
+  errors?: Record<string, string>;
+  message?: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface HttpError<T = any> extends AxiosError<T> {
+  message: string;
+  hasFieldErrors: boolean;
+  fieldErrors: Record<string, string | undefined>;
+}
+
+/**
+ *
+ * @param error @{AxiosError} to map to @{HttpError}
+ * @throws HttpError
+ */
+export async function formatErrorAndThrow<T>(
+  error: AxiosError<ErrorResponse>
+): Promise<void> {
+  const httpError = error as HttpError<T>;
+
+  // if (httpError.response?.status === StatusCode.UNAUTHORIZED) {
+  //   TokenUtils.dispatchSessionChanged(undefined);
+  //   Toast.show("Session expired. Please login again!");
+  //   return;
+  // }
+
+  // const netInfo = await getNetInfo();
+  // if (netInfo?.isConnected === false) {
+  //   httpError.fieldErrors = {};
+  //   httpError.hasFieldErrors = false;
+  //   httpError.message = i18next.t(Messages.NO_INTERNET_CONNECTION, { ns: "network" });
+  //   throw httpError;
+  // }
+
+  let message = "Something went wrong";
+  if (typeof error.response?.data?.errorMessage === "string") {
+    message = error.response.data.errorMessage;
+  } else if (typeof error.response?.data?.message === "string") {
+    message = error.response?.data.message;
+  }
+
+  // TODO: need to verify from backend and develop form error valid function
+  httpError.fieldErrors = error.response?.data?.errors || {};
+  httpError.hasFieldErrors = Object.keys(httpError.fieldErrors).length === 0;
+  httpError.message = message;
+  throw httpError;
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,69 @@
 <template>
-    
-    <main class="min-h-screen">
-        <div class="flex justify-center items-center h-screen">
-            <div class="max-w-md w-full">
-                <h2 class="text-3xl font-bold mb-6 text-center">Login</h2>
-                <form @submit.prevent="handleLoginClick" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <div class="mb-4">
-                        <label for="username" class="label">Username:</label>
-                        <input type="text" id="username" v-model="username" required
-                            class="input">
-                    </div>
-                    <div class="mb-6">
-                        <label for="password" class="label">Password:</label>
-                        <input type="password" id="password" v-model="password" required
-                            class="input">
-                    </div>
-                    <div class="flex items-center justify-between">
-                        <button type="submit"
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                            Login
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </main>
+  <main class="min-h-screen">
+    <div class="flex justify-center items-center h-screen">
+      <div class="max-w-md w-full">
+        <h2 class="text-3xl font-bold mb-6 text-center">Login</h2>
+        <form
+          @submit.prevent="handleLoginClick"
+          class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div class="mb-4">
+            <label for="username" class="label">Username:</label>
+            <input
+              type="text"
+              id="username"
+              v-model="username"
+              required
+              class="input"
+            />
+          </div>
+          <div class="mb-6">
+            <label for="password" class="label">Password:</label>
+            <input
+              type="password"
+              id="password"
+              v-model="password"
+              required
+              class="input"
+            />
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              type="submit"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </main>
 </template>
-  
-  
+
 <script lang="ts" setup>
-import { useRouter } from 'nuxt/app';
+import { useRouter } from "nuxt/app";
+import { Api } from "../api/api";
 
 const router = useRouter();
 
-const username = ref('');
-const password = ref('');
+const username = ref("");
+const password = ref("");
 
-const handleLoginClick = () => {
+const handleLoginClick = async () => {
+  if (username.value && password.value) {
+    const data = await Api.post("/login", {
+      email: username.value,
+      password: password.value,
+    });
 
-    if (username.value === 'admin' && password.value === 'admin') {
-        localStorage.setItem("token", "1234567890")
-        router.replace({ path: "/list" });
+    if (data) {
+      localStorage.setItem("userDetails", JSON.stringify(data.data));
+      router.replace({ path: "/list" });
     }
-    // Clear the form fields after login
-    username.value = '';
-    password.value = '';
-}
-
+  }
+  // Clear the form fields after login
+  username.value = "";
+  password.value = "";
+};
 </script>
